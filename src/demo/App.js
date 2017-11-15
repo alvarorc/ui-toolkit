0c26cb35b68f9dc0fb7503abe0fefc18e55a5deb
Alvaro Rago <alvarorc.sistemas@gmail.com>
2017-11-15 18:34:35 -0300

    Add menu

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-grid-system';
 import Button from 'components/Button';
+import Menu from 'components/Menu';
+
+import './app.css';
 
 const App = () => (
-  <div className="App">
-    <div title="This is a eg proyect base setup">
-      <Button primary>From demo</Button>
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </div>
+  <div styleName="app">
+    <Menu fixed>
+      <Menu.Item>Some place</Menu.Item>
+      <Menu.Menu pullRight>
+        <Menu.Item>Group item</Menu.Item>
+        <Menu.Item>Group item</Menu.Item>
+        <Menu.Item>Group item</Menu.Item>
+      </Menu.Menu>
+    </Menu>
+    <Container fluid styleName="padded">
+      <Row>
+        <Col sm={4}>
+          <Button primary>From demo</Button>
+        </Col>
+        <Col sm={8}>
+          center column To get started, edit <code>src/App.js</code> and save to reload.
+        </Col>
+      </Row>
+    </Container>
   </div>
 );
 
